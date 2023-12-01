1bc569ca3b77e71cf31ee36fb164395c4b012b1d
Sven Eric Kugler <skugler.mmt-b2022@fh-salzburg.ac.at>
2023-12-01 11:38:44 +0100

    refactored helper added

---
+++ refactored.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Benutzereingabe für den Tag
+echo "Geben Sie den Tag für Advent of Code ein:"
+read -r day_number
+
+# Advent of Code Ordner erstellen
+folder_name="adventofcode${day_number}"
+
+# Folder zu Git hinzufügen
+git add $folder_name
+
+# Commit erstellen
+git commit -m "finished and working - day $day_number - refactored"
+
+# Den Commit pushen
+git push
+
+# Nachricht ausgeben
+echo "Advent of Code Projekt für Tag $day_number gepusht!"
