b24f9f1db255607298e10a67afaa9443d7f2c089
Sven Eric Kugler <skugler.mmt-b2022@fh-salzburg.ac.at>
2023-12-04 09:23:00 +0100

    partial working - day 04

--- adventofcode04/Solution.cs
+++ adventofcode04/Solution.cs
@@ -1,19 +1,112 @@
+using System.Diagnostics;
+
 namespace adventofcode2023 
 {
     public class Solution
     {
         public string SolutionOfFirstPart(string[] lines)
         {
-            string solution;
+            bool wantToDebug = true;
+            int solution = 0;
+            List<int> winningNumbers = new();
 
-            // Hier kommt die Logik für den ersten Teil hin
+            foreach (string game in lines)
+            {
+                string[] numbers = game.Split(" ");
 
-            return solution;
+                FillListWithWinningNumbers(ref winningNumbers, numbers);
+
+                solution += CalculatePoints(winningNumbers, numbers);
+
+                ClearList(ref winningNumbers);
+            }
+            return solution.ToString();
         }
+
+        private static void ClearList(ref List<int> winningNumbers)
+        {
+            winningNumbers = new();
+        }
+
+        private static int CalculatePoints(List<int> winningNumbers, string[] numbers)
+        {
+            bool foundRegularNumbers = false;
+            int points = 0;
+            foreach (string number in numbers)
+            {
+                if(foundRegularNumbers && number != "")
+                {
+                    if (CheckForWin(ref winningNumbers, number))
+                    {
+                        if(points == 0)
+                        {
+                            points = 1;
+                        }
+                        else
+                        {
+                            points *= 2;
+                        }
+                    }
+                }
+                else
+                {
+                    if (number == "|")
+                    {
+                        foundRegularNumbers = true;
+                    }
+                }
+            }
+            return points;
+        }
+
+        private static bool CheckForWin(ref List<int> winningNumbers, string number)
+        {
+            foreach (int winningNumber in winningNumbers)
+            {
+                if (number == winningNumber.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static void FillListWithWinningNumbers(ref List<int> winningNumbers, string[] numbers)
+        {
+            bool foundWinningNumbers = false;
+            foreach (string number in numbers)
+            {
+                if(foundWinningNumbers)
+                {
+                    if (number == "|")
+                    {
+                        break;
+                    }
+                    if (number != "")
+                    {
+                        winningNumbers.Add(int.Parse(number));
+                    }
+                }
+                else
+                {
+                    if (number.EndsWith(":"))
+                    {
+                        foundWinningNumbers = true;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
 
         public string SolutionOfSecondPart(string[] lines)
         {
-            string solution;
+            string solution = "";
 
             // Hier kommt die Logik für den zweiten Teil hin
 
