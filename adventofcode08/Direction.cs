695160a654f4aaa542b6ccf9d7f11c776fa783d1
Sven Eric Kugler <skugler.mmt-b2022@fh-salzburg.ac.at>
2023-12-08 10:08:20 +0100

    partial working - day 08

---
+++ adventofcode08/Direction.cs
@@ -0,0 +1,8 @@
+namespace AdventOfCode2023
+{
+    public class Direction
+    {
+        public required string Left { get; set; }
+        public required string Right { get; set; }
+    }
+}
--- adventofcode08/Solution.cs
+++ adventofcode08/Solution.cs
@@ -1,19 +1,76 @@
+
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using AdventOfCode2023;
+
 namespace adventofcode2023 
 {
     public class Solution
     {
         public string SolutionOfFirstPart(string[] lines)
         {
-            string solution;
+            int solution = 0;
 
-            // Hier kommt die Logik für den ersten Teil hin
+            char[] directionInstruction = GenerateDirectionInstruction(ref lines[0]);
+            Dictionary<string, Direction> map = GenerateDirectionMap(ref lines);
 
-            return solution;
+            GetTroughtTheMap(ref directionInstruction, ref map, ref solution);
+
+            return solution.ToString();
+        }
+
+        private bool GetTroughtTheMap(ref char[] directionInstruction, ref Dictionary<string, Direction> map, ref int solution, string location = "AAA", int index = 0)
+        {
+            if(index == directionInstruction.Length)
+            {
+                index = 0;
+            }
+            if (location == "ZZZ")
+            {
+                return true;
+            }
+            else 
+            {
+                solution++;
+                if (directionInstruction[index] == 'L')
+                {
+                    return GetTroughtTheMap(ref directionInstruction, ref map, ref solution, map[location].Left, index + 1);
+                }
+                else
+                {
+                    return GetTroughtTheMap(ref directionInstruction, ref map, ref solution, map[location].Right, index + 1);
+                }
+            }
+        }
+
+        private Dictionary<string, Direction> GenerateDirectionMap(ref string[] lines)
+        {
+            Dictionary<string, Direction> map = new();
+            for (int i = 2; i < lines.Length; i++)
+            {
+                AddThisLine(lines[i], ref map);
+            }
+            return map;
+        }
+
+        private void AddThisLine(string line, ref Dictionary<string, Direction> map)
+        {
+            map.Add(line[..3], new Direction
+            {
+                Left = line.Substring(7, 3),
+                Right = line.Substring(12, 3)
+            });
+            Debug.WriteLine(line[..3] + " " + line.Substring(7, 3) + " " + line.Substring(12, 3));
+        }
+
+        private char[] GenerateDirectionInstruction(ref string line)
+        {
+            return line.ToCharArray();
         }
 
         public string SolutionOfSecondPart(string[] lines)
         {
-            string solution;
+            string solution = "";
 
             // Hier kommt die Logik für den zweiten Teil hin
 
