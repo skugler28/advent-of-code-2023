5d5d95e2d258e9118bc406470b6f774117f86611
Sven Eric Kugler <skugler.mmt-b2022@fh-salzburg.ac.at>
2023-12-09 12:12:05 +0100

    finished and working - day 09 - refactored

--- adventofcode09/Solution.cs
+++ adventofcode09/Solution.cs
@@ -1,23 +1,51 @@
+
 namespace adventofcode2023 
 {
     public class Solution
     {
         public string SolutionOfFirstPart(string[] lines)
         {
-            string solution;
-
-            // Hier kommt die Logik für den ersten Teil hin
-
-            return solution;
+            return Solve(lines, ExtrapolateRight).ToString();
         }
-
         public string SolutionOfSecondPart(string[] lines)
         {
-            string solution;
+            return Solve(lines, ExtrapolateLeft).ToString();
+        }
 
-            // Hier kommt die Logik für den zweiten Teil hin
 
-            return solution;
+
+        public static long Solve(string[] lines, Func<long[], long> extrapolate)
+        {
+            long[][] parsedNumbers = lines.Select(ParseNumbers).ToArray();
+            long[] extrapolatedNumbers = parsedNumbers.Select(extrapolate).ToArray();
+
+            return extrapolatedNumbers.Sum();
+        }
+
+        public static long[] ParseNumbers(string line)
+        {
+            long[] parsedValues = line.Split(" ").Select(long.Parse).ToArray();
+            return parsedValues;
+        }
+
+        public static long[] Diff(long[] numbers)
+        {
+            long[] differences = numbers.Zip(numbers.Skip(1), (long first, long second) => second - first).ToArray();
+            return differences;
+        }
+
+        public long ExtrapolateRight(long[] numbers)
+        {
+            if (numbers.Length == 0) return 0;
+
+            long[] differences = Diff(numbers);
+            return ExtrapolateRight(differences) + numbers.Last();
+        }
+
+        public long ExtrapolateLeft(long[] numbers)
+        {
+            long[] reversedNumbers = numbers.Reverse().ToArray();
+            return ExtrapolateRight(reversedNumbers);
         }
     }
 }
